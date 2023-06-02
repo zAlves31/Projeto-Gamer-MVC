d88d7416fb718c9503f51224e450dd1feaf11f65
João Victor Alves Bautista <52546124870@info.local>
2023-06-02 10:55:42 -0300

    02.06

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -85,7 +85,7 @@ namespace Projeto_Gamer_manha.Controllers
         [Route("Exluir/{id}")]
         public IActionResult Excluir(int id)
         {
-            Equipe equipeBuscada = c.Equipe.FirstOrDefault(e=> e.IdEquipe == id);
+            Equipe equipeBuscada = c.Equipe.FirstOrDefault(e => e.IdEquipe == id);
 
             c.Remove(equipeBuscada);
 
@@ -93,5 +93,65 @@ namespace Projeto_Gamer_manha.Controllers
 
             return LocalRedirect("~/Equipe/Listar");
         }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            Equipe equipe = c.Equipe.First(x => x.IdEquipe == id);
+
+            ViewBag.Equipe = equipe;
+
+            return View("Edit");
+        } 
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection form)
+        {
+            Equipe equipe = new Equipe ();
+
+            equipe.IdEquipe = int.Parse(form["IdEquipe"].ToString());
+
+            equipe.Nome = form["Nome"].ToString();
+
+             if (form.Files.Count > 0)
+            {
+                
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                // gerar o caminho completo ate o caminho do arquivo(imagem - nome com extensao)
+                var path = Path.Combine(folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                equipe.Imagem = file.FileName;
+
+            }
+            else
+            {
+                equipe.Imagem = "padrao.png";
+            }
+
+            Equipe equipeBuscada = c.Equipe.First(x => x.IdEquipe == equipe.IdEquipe);
+
+            equipeBuscada.Nome = equipe.Nome;
+            equipeBuscada.Imagem = equipe.Imagem;
+
+            c.Equipe.Update(equipeBuscada);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/Listar");
+
+        }    
     }
 }
---
+++ Controllers/JogadorController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Projeto_Gamer_manha.Infra;
+
+namespace Projeto_Gamer_manha.Controllers
+{
+    [Route("[controller]")]
+    public class JogadorController : Controller
+    {
+        private readonly ILogger<JogadorController> _logger;
+
+        public JogadorController(ILogger<JogadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        Context c = new Context();
+
+        [Route("Listar")]
+        public IActionResult Index()
+        {
+            ViewBag.Jogador = c.Jogador.ToList();
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
