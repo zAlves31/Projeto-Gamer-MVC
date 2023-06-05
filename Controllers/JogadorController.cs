50708b60c4ece8089dd7f57515d8e32743c24f0b
João Victor Alves Bautista <52546124870@info.local>
2023-06-05 08:50:13 -0300

    05.06

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Projeto_Gamer_manha.Infra;
+using Projeto_Gamer_manha.Models;
 
 namespace Projeto_Gamer_manha.Controllers
 {
@@ -30,10 +31,50 @@ namespace Projeto_Gamer_manha.Controllers
             return View();
         }
 
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar (IFormCollection form)
+        {
+            // instancia do objeto equipe
+            Jogador novoJogador = new Jogador();
+
+            //atribuicao de valores recebidos do formulario 
+            novoJogador.Nome = form["Nome"].ToString();
+
+            novoJogador.Nome = form["Email"].ToString();
+
+            novoJogador.Nome = form["Senha"].ToString();
+
+            return LocalRedirect("~/Jogador");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
         }
+
+         [Route("Exluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            Jogador jogadorBuscado = c.Jogador.FirstOrDefault(e => e.IdJogador == id);
+
+            c.Remove(jogadorBuscado);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Jogador");
+        }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            Jogador jogador = c.Jogador.First(x => x.IdJogador == id);
+
+            ViewBag.Jogador = jogador;
+
+            return View("Edit");
+        } 
+
+        
     }
 }
